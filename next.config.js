6c0e7bfb47a631c3cecc908d7d358080a73bcf5e
Preston Harris <preston@point-of-rental.com>
2025-08-14 09:19:54 -0500

    🔧 Fix Vercel build: Remove contentlayer dependencies
    - Remove next-contentlayer and contentlayer packages that don't support Next.js 14
    - Remove @next/mdx and @types/mdx dependencies
    - Update next.config.js to remove contentlayer integration
    - Update tsconfig.json to remove contentlayer references
    - Remove contentlayer build script
    
    This fixes the ERESOLVE dependency conflict preventing Vercel deployment.
    MDX/Contentlayer can be added later when Next.js 14 support is available.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { withContentlayer } = require('next-contentlayer')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -51,4 +49,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withContentlayer(nextConfig)
+module.exports = nextConfig
